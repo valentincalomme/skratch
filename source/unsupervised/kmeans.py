8e69cac7932d11bfdc28edc947db5b5abcb7325d
Valentin Calomme <vca@mediaan.com>
2018-06-04 14:25:27 +0200

    added kmeans

--- source/unsupervised/kmeans.py
+++ source/unsupervised/kmeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 from utils.distances import euclidean
 
@@ -8,14 +9,12 @@ class KMeans:
     def __init__(self, k=3, seed=0, n_runs=10):
 
         self.k = k
-        self.seed = seed
-        np.random.seed(self.seed)
-
+        self.rnd = np.random.RandomState(seed)
         self.n_runs = n_runs
 
     def predict(self, X):
 
-        pass
+        return self._compute_labels(X, self.centroids_)
 
     def fit(self, X, y=None):
 
@@ -23,30 +22,103 @@ class KMeans:
 
         for run in range(self.n_runs):
 
-            centroids = self._initialize_centroids(X)
+            for c, l in self._fit(X, y):
+                centroids, labels = c, l
 
-            for labels_, centroids_ in self._update_centroids(X, centroids):
+            performance = self._performance(X, centroids, labels)
 
-                
+            if performance < self.score_:
 
-
-        self.centroids_ = np.array([[]])
-
-        self.labels_ = np.array([])
+                self.score_ = performance
+                self.centroids_ = centroids
+                self.labels_ = labels
 
         return self
+
+    def _fit(self, X, y=None):
+
+        centroids = self._initialize_centroids(X)
+        labels = self._compute_labels(X, centroids)
+        old_labels = np.full_like(labels, -1)
+
+        while any(old_labels != labels):
+
+            old_labels = labels
+            centroids = self._compute_centroids(X, labels)
+            labels = self._compute_labels(X, centroids)
+
+            yield centroids, labels
+
+    def _initialize_centroids(self, X):
+
+        self.rnd.shuffle(X.copy())
+        return X[:self.k]
 
     def _distance(self, a, b):
 
         return euclidean(a, b)
 
-    def _update_centroids(self):
-        pass
+    def _compute_centroids(self, X, labels):
 
-    def _initialize_centroids(self, X):
+        centroids = []
 
-        pass
+        for i in range(self.k):
 
-    def _evaluate_clustering(self):
+            centroid = np.mean(X[labels == i], axis=0)
+            centroids.append(centroid)
 
-        pass
+        return np.array(centroids)
+
+    def _compute_labels(self, X, centroids):
+
+        labels = []
+
+        for x in X:
+
+            distances = [self._distance(x, centroid) for centroid in centroids]
+            label = np.argmin(distances)
+            labels.append(label)
+
+        return np.array(labels)
+
+    def _performance(self, X, centroids, labels):
+
+        distances = []
+
+        for i, centroid in enumerate(centroids):
+
+            distances.extend([self._distance(x, centroid) for x in X[labels == i]])
+
+        return np.mean(np.power(distances, 2))
+
+if __name__ == "__main__":
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+    from sklearn.datasets import make_blobs
+
+    # X, _ = make_blobs(n_samples=1000, n_features=2, centers=5)
+    X = np.random.rand(1000, 2)
+
+    kmeans = KMeans(k=3)
+    colors = np.random.rand(kmeans.k, 3)
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111)
+
+    ims = []
+
+    for c, l in kmeans._fit(X):
+
+        lines = []
+
+        for i in range(kmeans.k):
+
+            line, = ax.plot(X[l == i][:, 0], X[l == i][:, 1], '.', c=colors[i])
+
+        line, = ax.plot(c[:, 0], c[:, 1], 'X', c='black', ms=10)
+        ims.append(ax.lines[-(kmeans.k + 1):])
+
+    anim = animation.ArtistAnimation(fig, ims, interval=200, blit=True, repeat_delay=100)
+    plt.show()
