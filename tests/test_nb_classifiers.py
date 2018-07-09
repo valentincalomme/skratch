ac97a03b01bef725ebece84f70954d46c8f5c869
Valentin Calomme <vca@mediaan.com>
2018-07-09 12:40:36 +0200

    refactored code in test_nb_classifiers.py

--- tests/test_nb_classifiers.py
+++ tests/test_nb_classifiers.py
@@ -4,19 +4,15 @@ from itertools import product
 from sklearn import datasets
 import numpy as np
 
-from supervised.naive_bayes.gaussian_nb import GaussianNB
-from sklearn.naive_bayes import GaussianNB as sklearn_gnb
-
-from supervised.naive_bayes.bernoulli_nb import BernoulliNB
-from sklearn.naive_bayes import BernoulliNB as sklearn_BernoulliNB
-
-from supervised.naive_bayes.multinomial_nb import MultinomialNB
-from sklearn.naive_bayes import MultinomialNB as sklearn_MultinomialNB
 
 EPSILON = 5E-2
 N_SAMPLES = [500]
 N_DIMS = range(2, 10)
 N_CLASSES = range(2, 5)
+
+
+from supervised.naive_bayes.gaussian_nb import GaussianNB
+from sklearn.naive_bayes import GaussianNB as sklearn_gnb
 
 
 @pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
@@ -35,23 +31,6 @@ def test_gaussian_vs_sklearn(n_samples, n_dims, n_classes):
 
 
 @pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
-def test_bernoulli_vs_sklearn(n_samples, n_dims, n_classes):
-
-    X = np.random.randint(2, size=(n_samples, n_dims))
-    y = np.random.randint(0, n_classes, size=(n_samples,))
-
-    clf1 = BernoulliNB()
-    clf2 = sklearn_BernoulliNB()
-
-    y_pred1 = clf1.fit(X, y).predict(X)
-    y_pred2 = clf2.fit(X, y).predict(X)
-
-    assert np.mean(y_pred1 == y_pred2) >= 1 - EPSILON
-
-
-
-
-@pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
 def test_gaussian_is_not_stochastic(n_samples, n_dims, n_classes):
 
     X = np.random.rand(n_samples, n_dims)
@@ -64,6 +43,24 @@ def test_gaussian_is_not_stochastic(n_samples, n_dims, n_classes):
     y_pred2 = clf2.fit(X, y).predict(X)
 
     assert all(y_pred1 == y_pred2)
+
+from supervised.naive_bayes.bernoulli_nb import BernoulliNB
+from sklearn.naive_bayes import BernoulliNB as sklearn_BernoulliNB
+
+
+@pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
+def test_bernoulli_vs_sklearn(n_samples, n_dims, n_classes):
+
+    X = np.random.randint(2, size=(n_samples, n_dims))
+    y = np.random.randint(0, n_classes, size=(n_samples,))
+
+    clf1 = BernoulliNB()
+    clf2 = sklearn_BernoulliNB()
+
+    y_pred1 = clf1.fit(X, y).predict(X)
+    y_pred2 = clf2.fit(X, y).predict(X)
+
+    assert np.mean(y_pred1 == y_pred2) >= 1 - EPSILON
 
 
 @pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
@@ -80,6 +77,9 @@ def test_bernoulli_is_not_stochastic(n_samples, n_dims, n_classes):
 
     assert all(y_pred1 == y_pred2)
 
+
+# from supervised.naive_bayes.multinomial_nb import MultinomialNB
+# from sklearn.naive_bayes import MultinomialNB as sklearn_MultinomialNB
 
 # @pytest.mark.parametrize("n_samples, n_dims, n_classes", product(N_SAMPLES, N_DIMS, N_CLASSES))
 # def test_multinomial_vs_sklearn(n_samples, n_dims, n_classes):
