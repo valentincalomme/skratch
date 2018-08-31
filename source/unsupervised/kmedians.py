91c9a0dfc3a0097aecab0055e3bb2f96f7dd3afe
Valentin Calomme <vca@mediaan.com>
2018-08-31 08:35:15 +0200

    removed unnecessary __init__

--- source/unsupervised/kmedians.py
+++ source/unsupervised/kmedians.py
@@ -7,10 +7,6 @@ from utils.distances import manhattan
 
 class KMedians(KMeans):
 
-    def __init__(self, k=3, seed=None, n_runs=10, max_iters=300):
-
-        super().__init__(k=k, seed=seed, n_runs=n_runs, max_iters=max_iters)
-
     def _distance(self, a, b):
 
         return manhattan(a, b)
