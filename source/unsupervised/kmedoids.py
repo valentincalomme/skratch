0c7376e22e078ca2a3b7b6de65235d01c217ab1a
Valentin Calomme <vca@mediaan.com>
2018-08-31 08:34:47 +0200

    fixed distance storage

--- source/unsupervised/kmedoids.py
+++ source/unsupervised/kmedoids.py
@@ -7,21 +7,20 @@ from utils.distances import pdist, euclidean, manhattan, cosine
 
 class KMedoids(KMeans):
 
-    def __init__(self, k=3, seed=None, n_runs=10, max_iters=300):
-
-        super().__init__(k=k, seed=seed, n_runs=n_runs, max_iters=max_iters)
-
     def _distance(self, a, b):
 
-        return euclidean(a, b)
+        return cosine(a, b)
 
     def _compute_centroids(self, X, labels):
+
+        if not hasattr(self, "distances"):
+            self.distances = pdist(X, self._distance)
 
         centroids = []
 
         for i in range(self.k):
 
-            distances = pdist(X[labels == i], self._distance)
+            distances = self.distances[np.ix_(labels == i, labels == i)]
             within_cluster_sum_of_distances = np.sum(distances, axis=0)
 
             centroid = X[labels == i][np.argmin(within_cluster_sum_of_distances)]
